53412f6f62a47849ea2227c06eefbd2236f92905
mperez <marcelo.perez@willdom.com>
2023-08-07 00:31:40 -0400

    feat: add toast

--- src/app/[id]/Story.tsx
+++ src/app/[id]/Story.tsx
@@ -8,10 +8,15 @@ import { store } from "@/redux/store";
 import { setStory } from "@/redux/features/selectedStorySlice";
 import { Storage } from "aws-amplify";
 import awsSetup from "../../aws-exports";
+import { TailwindToaster } from "@/components/Toast";
+import { toast } from "react-hot-toast";
+import React from "react";
+
 Storage.configure({ awsSetup });
 
 export const Story = ({ story: data }: { story: IStory }) => {
   const { back } = useRouter();
+  const [uploading, setUploading] = React.useState(false);
   const story = useAppSelector((state) => state.storyReducer);
   useEffect(() => {
     store.dispatch(setStory(data));
@@ -20,14 +25,20 @@ export const Story = ({ story: data }: { story: IStory }) => {
   }, []);
 
   const handleUploadToS3 = async () => {
+    setUploading(true);
+
     try {
       const data = await Storage.put(`${story.id}.txt`, story.body, {
         level: "public",
         contentType: "text/plain",
       });
       console.log("File uploaded successfully:", data);
+      toast.success("Uploading to S3");
     } catch (error) {
+      toast.error("Error uploading file to S3");
       console.error("Error uploading file:", error);
+    } finally {
+      setUploading(false);
     }
   };
 
@@ -45,12 +56,16 @@ export const Story = ({ story: data }: { story: IStory }) => {
         <p className="text-justify text-sm text-gray-600 italic">{`(Prompt used: ${story?.prompt})`}</p>
         <p className="my-20 text-justify">{story?.body}</p>
         <button
-          className="bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-md"
+          disabled={uploading}
+          className={`${uploading ? "bg-green-200" : "bg-green-500"} ${
+            !uploading && "hover:bg-green-600"
+          } text-white px-4 py-2 rounded-md`}
           onClick={handleUploadToS3}
         >
           Upload to S3
         </button>
       </div>
+      <TailwindToaster />
     </div>
   );
 };
