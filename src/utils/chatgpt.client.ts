399994e7e2c8efceec739afba3c8c05871cdae99
mperez <marcelo.perez@willdom.com>
2023-08-06 23:10:37 -0400

    chore: error management

--- src/utils/chatgpt.client.ts
+++ src/utils/chatgpt.client.ts
@@ -1,28 +1,29 @@
 import { Configuration, OpenAIApi } from "openai";
-import { cache } from "react";
 
-export const getChatResponse = cache(async (prompt: string) => {
+export const getChatResponse = async (prompt: string) => {
   const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
   });
   const openai = new OpenAIApi(configuration);
+  try {
+    const response = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
 
-  const response = await openai.createChatCompletion({
-    model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "user",
+          content: `Give me a story with a maximum of 100 words using this prompt: \n${prompt}`,
+        },
+      ],
+      temperature: 1,
+      max_tokens: 256,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+    });
 
-    messages: [
-      {
-        role: "user",
-        content:
-          "Give me a story with a maximum of 100 words using this prompt: \n{{texto}}",
-      },
-    ],
-    temperature: 1,
-    max_tokens: 256,
-    top_p: 1,
-    frequency_penalty: 0,
-    presence_penalty: 0,
-  });
-
-  return response.data.choices[0].message?.content;
-});
+    return response.data.choices[0].message?.content;
+  } catch (e) {
+    return `${JSON.stringify(e)} ${process.env.OPENAI_API_KEY}}`;
+  }
+};
